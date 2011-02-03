3e0f76845a5154306ae057499d318ae9b8a1716e
Kucko <Kucko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-03 14:35:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sticky-note/trunk@340340 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -10,7 +10,7 @@ $k_note=$_POST['k_note'];
 $wpdb->query( $wpdb->query( "UPDATE $table_name SET note = ('$k_note') WHERE id = '1'"  ) );
 
 if($wpdb) {
-	header("location:/wp-admin/options-general.php?page=plugin");
+	header("location:/wp-admin/options-general.php?page=sticky-note");
 	exit();
 }
 
